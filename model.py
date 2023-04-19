0254b4bd0605dfb156363e68c5525a5fb3caf025
benhduong <benhduong@gmail.com>
2023-04-18 21:30:11 -0400

    fixed model

--- model.py
+++ model.py
@@ -2,7 +2,18 @@ from __future__ import absolute_import
 from matplotlib import pyplot as plt
 from preprocess import get_data
 from convolution import conv2d
+from keras.layers import Conv2D, UpSampling2D, InputLayer, Conv2DTranspose
+from keras.layers import Activation, Dense, Dropout, Flatten
+from keras.layers.normalization import BatchNormalization
+from keras.models import Sequential
+from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array, load_img
+from skimage.color import rgb2lab, lab2rgb, rgb2gray, xyz2lab
+from skimage.io import imsave
 
+import numpy as np
+import os
+import random
+import tensorflow as tf
 import os
 import tensorflow as tf
 import numpy as np
@@ -18,30 +29,9 @@ class Model(tf.keras.Model):
 
         self.batch_size = 64
         self.num_classes = 2
-        self.loss_list = [] # Append losses to this list in training so you can visualize loss vs time in main
 
-        # TODO: Initialize all hyperparameters
         self.e = 1e-5
-        
-        # TODO: Initialize all trainable parameters
-        def make_variables(*dims, initializer=tf.random.truncated_normal): 
-            return tf.Variable(initializer(dims, stddev=0.1, dtype=tf.float32), dtype=tf.float32)
-        
-        self.w1 = make_variables(80, 64)
-        self.b1 = make_variables(64)
-        self.w2 = make_variables(64, 32)
-        self.b2 = make_variables(32)
-        self.w3 = make_variables(32, 2)
-        self.b3 = make_variables(2)
-
-        self.filter1 = make_variables(5, 5, 3, 16)
-        self.filter2 = make_variables(5, 5, 16, 20)
-        self.filter3 = make_variables(3, 3, 20, 20)
-
-        self.bias1 = make_variables(16)
-        self.bias2 = make_variables(20)
-        self.bias3 = make_variables(20)
-        
+         
     def call(self, inputs, is_testing=False):
         """
         Runs a forward pass on an input batch of images.
@@ -50,44 +40,8 @@ class Model(tf.keras.Model):
         :param is_testing: a boolean that should be set to True only when you're doing Part 2 of the assignment and this function is being called during testing
         :return: logits - a matrix of shape (num_inputs, num_classes); during training, it would be (batch_size, 2)
         """
-        # Remember that
-        # shape of input = (num_inputs (or batch_size), in_height, in_width, in_channels)
-        # shape of filter = (filter_height, filter_width, in_channels, out_channels)
-        # shape of strides = (batch_stride, height_stride, width_stride, channels_stride)
-        
-        inputs = tf.cast(inputs, tf.float32)
-        conv1 = tf.nn.conv2d(inputs, self.filter1, [2, 2], padding='SAME')
-        c1out = tf.nn.bias_add(conv1, self.bias1)
-        mean1, variance1 = tf.nn.moments(c1out, axes=[0,1,2])
-        batch1 = tf.nn.batch_normalization(conv1, mean1, variance1, None, None, self.e)
-        relu1 = tf.nn.relu(batch1)
-        maxpool1 = tf.nn.max_pool(relu1, ksize=3, strides=2, padding="SAME")
-        conv2 = tf.nn.conv2d(maxpool1, self.filter2, [2, 2], padding="SAME")
-        c2out = tf.nn.bias_add(conv2, self.bias2)
-        mean2, variance2 = tf.nn.moments(c2out, axes=[0,1,2])
-        batch2 = tf.nn.batch_normalization(conv2, mean2, variance2, None, None, self.e)
-        relu2 = tf.nn.relu(batch2)
-        maxpool2 = tf.cast(tf.nn.max_pool(relu2, ksize=2, strides=2, padding="SAME"), tf.float32)
-        if is_testing:
-            conv3 = conv2d(maxpool2, self.filter3, [1, 1, 1, 1], "SAME")
-        else:
-            conv3 = tf.nn.conv2d(maxpool2, self.filter3, [1, 1], padding="SAME")
-        c3out = tf.nn.bias_add(conv3, self.bias3)
-        mean3, variance3 = tf.nn.moments(c3out, axes=[0,1,2])
-        batch3 = tf.nn.batch_normalization(conv3, mean3, variance3, None, None, self.e)
-        relu3 = tf.nn.relu(batch3)
 
-        x = tf.cast(tf.reshape(relu3, [len(inputs), -1]), tf.float32)
-
-        dense1 = tf.matmul(x, self.w1) + self.b1
-        rd1 = tf.nn.relu(dense1)
-        drop1 = tf.cast(tf.nn.dropout(rd1, rate=0.3), tf.float32)
-        dense2 = tf.matmul(drop1, self.w2) + self.b2
-        rd2 = tf.nn.relu(dense2)
-        drop2 = tf.cast(tf.nn.dropout(rd2, rate=0.3), tf.float32)
-        dense3 = tf.matmul(drop2, self.w3) + self.b3
-
-        return dense3
+        return
 
     def loss(self, logits, labels):
         """
@@ -120,19 +74,6 @@ class Model(tf.keras.Model):
         return tf.reduce_mean(tf.cast(correct_predictions, tf.float32))
 
 def train(model, train_inputs, train_labels):
-    '''
-    Trains the model on all of the inputs and labels for one epoch. You should shuffle your inputs 
-    and labels - ensure that they are shuffled in the same order using tf.gather or zipping.
-    To increase accuracy, you may want to use tf.image.random_flip_left_right on your
-    inputs before doing the forward pass. You should batch your inputs.
-    
-    :param model: the initialized model to use for the forward pass and backward pass
-    :param train_inputs: train inputs (all inputs to use for training), 
-    shape (num_inputs, width, height, num_channels)
-    :param train_labels: train labels (all labels to use for training), 
-    shape (num_labels, num_classes)
-    :return: Optionally list of losses per batch to use for visualize_loss
-    '''
     index = list(range(len(train_inputs)))
     np.random.shuffle(index)
     tf.gather(train_inputs, index)
@@ -159,17 +100,6 @@ def train(model, train_inputs, train_labels):
 
 
 def test(model, test_inputs, test_labels):
-    """
-    Tests the model on the test inputs and labels. You should NOT randomly 
-    flip images or do any extra preprocessing.
-    
-    :param test_inputs: test data (all images to be tested), 
-    shape (num_inputs, width, height, num_channels)
-    :param test_labels: test labels (all corresponding labels),
-    shape (num_labels, num_classes)
-    :return: test accuracy - this should be the average accuracy across
-    all batches
-    """
     acc = 0
     batch_num = 0
     b = model.batch_size
@@ -184,99 +114,28 @@ def test(model, test_inputs, test_labels):
     acc /= batch_num
     return acc
 
-
-def visualize_loss(losses): 
-    """
-    Uses Matplotlib to visualize the losses of our model.
-    :param losses: list of loss data stored from train. Can use the model's loss_list 
-    field 
-
-    NOTE: DO NOT EDIT
-
-    :return: doesn't return anything, a plot should pop-up 
-    """
-    x = [i for i in range(len(losses))]
-    plt.plot(x, losses)
-    plt.title('Loss per batch')
-    plt.xlabel('Batch')
-    plt.ylabel('Loss')
-    plt.show()  
-
-
-def visualize_results(image_inputs, probabilities, image_labels, first_label, second_label):
-    """
-    Uses Matplotlib to visualize the correct and incorrect results of our model.
-    :param image_inputs: image data from get_data(), limited to 50 images, shape (50, 32, 32, 3)
-    :param probabilities: the output of model.call(), shape (50, num_classes)
-    :param image_labels: the labels from get_data(), shape (50, num_classes)
-    :param first_label: the name of the first class, "cat"
-    :param second_label: the name of the second class, "dog"
-
-    NOTE: DO NOT EDIT
-
-    :return: doesn't return anything, two plots should pop-up, one for correct results,
-    one for incorrect results
-    """
-    # Helper function to plot images into 10 columns
-    def plotter(image_indices, label): 
-        nc = 10
-        nr = math.ceil(len(image_indices) / 10)
-        fig = plt.figure()
-        fig.suptitle("{} Examples\nPL = Predicted Label\nAL = Actual Label".format(label))
-        for i in range(len(image_indices)):
-            ind = image_indices[i]
-            ax = fig.add_subplot(nr, nc, i+1)
-            ax.imshow(image_inputs[ind], cmap="Greys")
-            pl = first_label if predicted_labels[ind] == 0.0 else second_label
-            al = first_label if np.argmax(
-                image_labels[ind], axis=0) == 0 else second_label
-            ax.set(title="PL: {}\nAL: {}".format(pl, al))
-            plt.setp(ax.get_xticklabels(), visible=False)
-            plt.setp(ax.get_yticklabels(), visible=False)
-            ax.tick_params(axis='both', which='both', length=0)
-        
-    predicted_labels = np.argmax(probabilities, axis=1)
-    num_images = image_inputs.shape[0]
-
-    # Separate correct and incorrect images
-    correct = []
-    incorrect = []
-    for i in range(num_images): 
-        if predicted_labels[i] == np.argmax(image_labels[i], axis=0): 
-            correct.append(i)
-        else: 
-            incorrect.append(i)
-
-    plotter(correct, 'Correct')
-    plotter(incorrect, 'Incorrect')
-    plt.show()
-
-
 def main():
-    '''
-    Read in CIFAR10 data (limited to 2 classes), initialize your model, and train and 
-    test your model for a number of epochs. We recommend that you train for
-    10 epochs and at most 25 epochs. 
-    
-    CS1470 students should receive a final accuracy 
-    on the testing examples for cat and dog of >=70%.
-    
-    CS2470 students should receive a final accuracy 
-    on the testing examples for cat and dog of >=75%.
-    
-    :return: None
-    '''
-    model = Model()
+    X, Y = get_data()
+    model = Sequential()
+    model.add(InputLayer(input_shape=(None, None, 1)))
+    model.add(Conv2D(8, (3, 3), activation='relu', padding='same', strides=2))
+    model.add(Conv2D(8, (3, 3), activation='relu', padding='same'))
+    model.add(Conv2D(16, (3, 3), activation='relu', padding='same'))
+    model.add(Conv2D(16, (3, 3), activation='relu', padding='same', strides=2))
+    model.add(Conv2D(32, (3, 3), activation='relu', padding='same'))
+    model.add(Conv2D(32, (3, 3), activation='relu', padding='same', strides=2))
+    model.add(UpSampling2D((2, 2)))
+    model.add(Conv2D(32, (3, 3), activation='relu', padding='same'))
+    model.add(UpSampling2D((2, 2)))
+    model.add(Conv2D(16, (3, 3), activation='relu', padding='same'))
+    model.add(UpSampling2D((2, 2)))
+    model.add(Conv2D(2, (3, 3), activation='tanh', padding='same'))
+    model.compile(optimizer='rmsprop',loss='mse')
 
-    traininputs, trainlabels = get_data('../data/train', 3, 5)
-    testinputs, testlabels = get_data('../data/test', 3, 5)
-    
-    epochs = 5
-    for i in range(epochs):      
-        train(model, traininputs, trainlabels)
-        test(model, testinputs, testlabels)
-        print(f'Epoch {i}')
-    
+    model.fit(x=X, 
+    y=Y,
+    batch_size=1,
+    epochs=1000)
     return
 
 
