bf4e8e38aa2f11632dd478bfd2528dbd0a94c9df
benhduong <benhduong@gmail.com>
2023-04-23 19:10:48 -0400

    preprocess works and model trains

--- model.py
+++ model.py
@@ -1,119 +1,39 @@
 from __future__ import absolute_import
 from matplotlib import pyplot as plt
 from keras.layers import Conv2D, UpSampling2D, InputLayer
-from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array, load_img
 from skimage.color import rgb2lab, lab2rgb, rgb2gray
 from skimage.io import imsave
 from keras.models import Sequential
 
-from preprocess_v2 import process_image
-
+from keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.utils import array_to_img, img_to_array, load_img
 import tensorflow as tf
 import numpy as np
-import os
 import random
 import math
 import pickle
 
 # ensures that we run only on cpu
+import os
 os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
 
-class Model(tf.keras.Model):
-    def __init__(self):
-        super(Model, self).__init__()
 
-        self.batch_size = 64
-        self.num_classes = 2
-
-        self.e = 1e-5
-         
-    def call(self, inputs, is_testing=False):
-        """
-        Runs a forward pass on an input batch of images.
-        
-        :param inputs: images, shape of (num_inputs, 32, 32, 3); during training, the shape is (batch_size, 32, 32, 3)
-        :param is_testing: a boolean that should be set to True only when you're doing Part 2 of the assignment and this function is being called during testing
-        :return: logits - a matrix of shape (num_inputs, num_classes); during training, it would be (batch_size, 2)
-        """
-
-        return
-
-    def loss(self, logits, labels):
-        """
-        Calculates the model cross-entropy loss after one forward pass.
-        Softmax is applied in this function.
-        
-        :param logits: during training, a matrix of shape (batch_size, self.num_classes) 
-        containing the result of multiple convolution and feed forward layers
-        :param labels: during training, matrix of shape (batch_size, self.num_classes) containing the train labels
-        :return: the loss of the model as a Tensor
-        """
-        softmax = tf.nn.softmax_cross_entropy_with_logits(labels, logits)
-        softmax = tf.reduce_mean(softmax)
-        return softmax
-
-    def accuracy(self, logits, labels):
-        """
-        Calculates the model's prediction accuracy by comparing
-        logits to correct labels - no need to modify this.
-        
-        :param logits: a matrix of size (num_inputs, self.num_classes); during training, this will be (batch_size, self.num_classes)
-        containing the result of multiple convolution and feed forward layers
-        :param labels: matrix of size (num_labels, self.num_classes) containing the answers, during training, this will be (batch_size, self.num_classes)
-
-        NOTE: DO NOT EDIT
-        
-        :return: the accuracy of the model as a Tensor
-        """
-        correct_predictions = tf.equal(tf.argmax(logits, 1), tf.argmax(labels, 1))
-        return tf.reduce_mean(tf.cast(correct_predictions, tf.float32))
-
-def train(model, train_inputs, train_labels):
-    optimizer = tf.keras.optimizers.Adam(learning_rate=0.001)
-    
-    b = model.batch_size
-    for i in range(0, int((len(train_inputs)/b)-1)):
-        xBatch = train_inputs[i*b:(i+1)*b]
-        yBatch = train_labels[i*b:(i+1)*b]
-
-        xBatch = tf.image.random_flip_left_right(xBatch)
-
-        with tf.GradientTape() as tape:
-            logits = model.call(xBatch, is_testing=False)
-            loss = model.loss(logits, yBatch)
-            model.loss_list.append(loss)
-
-        grads = tape.gradient(loss, model.trainable_variables)
-        optimizer.apply_gradients(zip(grads, model.trainable_variables))
-
-    return model.loss_list
-
-
-def test(model, test_inputs, test_labels):
-    acc = 0
-    batch_num = 0
-    b = model.batch_size
-    for i in range(0, int((len(test_inputs)/b)-1)):
-        xBatch = test_inputs[i*b:(i+1)*b]
-        yBatch = test_labels[i*b:(i+1)*b]
-
-        logits = model.call(xBatch, is_testing=False)
-        acc += model.accuracy(logits, yBatch)
-        batch_num += 1
-
-    acc /= batch_num
-    return acc
+def load_data(file):
+    with open(f'data/{file}.p', 'rb') as data_file:
+        data = pickle.load(data_file)
+    return data
 
 
 def main():
-    with open('data/data.p', 'rb') as data_file:
-        data_dict = pickle.load(data_file)
-    train_images    = data_dict['train_images']
-    train_labels    = data_dict['train_labels']
-    test_images     = data_dict['test_images']
-    test_labels     = data_dict['test_labels']
+    train1_images = load_data('train1')
+    train1_labels = load_data('train1_labels')
+    print(train1_images.shape)
+    # train2_images = load_data('train2')
+    # train2_labels = load_data('train2_labels')
+    # train3_images = load_data('train3')
+    # train3_labels = load_data('train3_labels')
+    print('it works!!')
 
-    X, Y = process_image('data/Images')
     model = Sequential()
     model.add(InputLayer(input_shape=(None, None, 1)))
     model.add(Conv2D(8, (3, 3), activation='relu', padding='same', strides=2))
@@ -130,22 +50,14 @@ def main():
     model.add(Conv2D(2, (3, 3), activation='tanh', padding='same'))
     model.compile(optimizer='rmsprop',loss='mse')
 
-    model.fit(x=X, 
-    y=Y,
-    batch_size=1,
-    epochs=1000)
+    model.fit(x=train1_images, y=train1_labels, batch_size=1, epochs=1)
 
-    print(model.evaluate(X, Y, batch_size=1))
-    image = img_to_array(load_img('data/Images/667626_18933d713e.jpg'))
-    image = np.array(image, dtype=float)
-    X = rgb2lab(1.0/255*image)[:,:,0]
-    X = X.reshape(1, 256, 256, 1)
-
-    output = model.predict(X)
+    print(model.evaluate(train1_images, train1_labels, batch_size=1))
+    output = model.predict(train1_images)
     output *= 128
     # Output colorizations
     cur = np.zeros((256, 256, 3))
-    cur[:,:,0] = X[0][:,:,0]
+    cur[:,:,0] = train1_images[0][:,:,0]
     cur[:,:,1:] = output[0]
     imsave("img_result.png", lab2rgb(cur))
     imsave("img_gray_version.png", rgb2gray(lab2rgb(cur)))
--- preprocess_v2.py
+++ preprocess_v2.py
@@ -10,21 +10,6 @@ import random
 import os
 os.environ["KMP_DUPLICATE_LIB_OK"]="TRUE"
 
-# def shuffle(images):
-#     # shuffle images
-#     random.seed(0)
-#     random.shuffle(images)
-#     return images
-
-# def split(images):
-#     # randomly split examples into training and testing sets
-#     train = images[:6400]
-#     test = images[6400:]
-#     return train, test
-
-# def load_imgs(img):
-#     images = io.imread(img)
-#     return images
 
 def process_images(image_names, data_folder):
     train_images = []
@@ -43,74 +28,74 @@ def process_images(image_names, data_folder):
         
         # The Image class in the Pillow library is used to open images as an np array. 3 means RGB.
         with Image.open(img) as img: # print("img shape:", np.shape(img)): (333, 500, 3)
-            img_array = np.asarray(img.resize((256,256))) # print("img_array shape", np.shape(img_array)): (256, 256, 3)
+            img_array = np.array(img.resize((256,256))) # print("img_array shape", np.shape(img_array)): (256, 256, 3)
             
         # Convert RGB to LAB
-        img_lab_rs = color.rgb2lab(img_array) # Shape: (256, 256, 3) 
+        img_lab_rs = color.rgb2lab(1.0/255*img_array) # Shape: (256, 256, 3) 
         
         # Extract Lightness from LAB and convert to tensor 
         img_l_rs = img_lab_rs[:,:,0]
-        tens_rs_l = tf.convert_to_tensor(img_l_rs) # tens_rs_l tf.Tensor([256 256], shape=(2,), dtype=int32)
+        img_l_rs = img_l_rs.reshape((256, 256, 1))
+        # tens_rs_l = tf.convert_to_tensor(img_l_rs) # tens_rs_l tf.Tensor([256 256], shape=(2,), dtype=int32)
 
         # Extract A and B from LAB and convert to tensor 
         img_ab_rs = img_lab_rs[:,:,1:]
-        tens_rs_ab = tf.convert_to_tensor(img_ab_rs) # tens_rs_ab tf.Tensor([256 256 2], shape=(3,), dtype=int32)
+        img_ab_rs /= 128
+        # tens_rs_ab = tf.convert_to_tensor(img_ab_rs) # tens_rs_ab tf.Tensor([256 256 2], shape=(3,), dtype=int32)
         
-        train_images += [tens_rs_l]
-        train_labels += [tens_rs_ab]
+        train_images += [img_l_rs]
+        train_labels += [img_ab_rs]
 
     return train_images, train_labels
 
-def load_data(data_folder):
-    image_names = []
+def load_data(data_folder, batch):
     train_images = []
     train_labels = []
 
     # all_pics = io.ImageCollection(f'{data_folder}/Images/*.jpg', load_func=load_imgs)
 
-    # shuffled_pics = shuffle(list(all_pics))
-    
-    # for img_idx in range(len(all_pics)):
-    #     image_name = os.path.basename(all_pics.files[img_idx])
-    #     image_names.append(image_name)
-
     text_file_path = f'{data_folder}/captions.txt'
     with open(text_file_path) as file:
         examples = file.read().splitlines()[1:]
 
-    #map each image name to a list containing all 5 of its captons
     image_names_to_captions = {}
 
     for example in examples:
         img_name, caption = example.split(',', 1)
         image_names_to_captions[img_name] = image_names_to_captions.get(img_name, []) + [caption]
 
+    # shuffle images
     shuffled_images = list(image_names_to_captions.keys())
     random.seed(0)
     random.shuffle(shuffled_images)
-    test_image_names = shuffled_images[:1000]
-    train_image_names = shuffled_images[1000:]
 
-    train_images, train_labels = process_images(train_image_names, data_folder)
-    test_images, test_labels = process_images(test_image_names, data_folder)
+    if batch == 'train1':
+        data = shuffled_images[1000:3500]
+    elif batch == 'train2':
+        data = shuffled_images[3500:6000]
+    elif batch == 'train3':
+        data = shuffled_images[6000:]
+    else:
+        data = shuffled_images[:1000]
 
-    return dict(
-        train_images = np.array(train_images),
-        train_labels = np.array(train_labels),
-        test_images = np.array(test_images),
-        test_labels = np.array(test_labels)
-    )
+    images, labels = process_images(data, data_folder)
 
-def create_pickle(data_folder):
-    with open(f'{data_folder}/data.p', 'wb') as pickle_file:
-        pickle.dump(load_data(data_folder), pickle_file)
-    print(f'Data has been dumped into {data_folder}/data.p!')
+    create_pickle(np.array(images), batch)
+    create_pickle(np.array(labels), batch + '_labels')
+
+
+def create_pickle(data, file):
+    with open(f'data/{file}.p', 'wb') as pickle_file:
+        pickle.dump(data, pickle_file)
+    print(f'Data has been dumped into {file}.p!')
+
 
 if __name__ == '__main__':
     ## Download this and put the Images into your ../data directory
     ## Flickr 8k Dataset: https://www.kaggle.com/datasets/adityajn105/flickr8k?resource=download
     
-    # img = load_imgs('data_test/667626_18933d713e.jpg')
-    # test, train = process_image('data')
-    create_pickle('data')
+    load_data('data', 'train1')
+    # load_data('data', 'train2')
+    # load_data('data', 'train3')
+    # load_data('data', 'test')
     
